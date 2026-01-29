976d77e62f98f50524ac10e3f6cce0ab0beab8b2
Junaid-Dawar-88 <junaidiqbal@gmail.com>
2026-01-29 14:46:50 +0500

    ch

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  root: '.', // make sure this is pointing to the folder containing index.html
-});
