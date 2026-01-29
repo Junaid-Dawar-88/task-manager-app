4aa93b52ccffedb8527fce210cbe0d039af2707e
Junaid-Dawar-88 <junaidiqbal@gmail.com>
2026-01-29 12:36:30 +0500

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // this is what Vercel will use
-  }
-})
+  root: '.', // make sure this is pointing to the folder containing index.html
+});
